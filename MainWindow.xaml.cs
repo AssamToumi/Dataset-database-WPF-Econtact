f1fb5d41b144eb6fa053bdff68b185421cfbcc68
Assam Toumi <assamtomi@outlook.com>
2020-07-03 21:25:03 +0100

    done!

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+
+
+namespace Econtact
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        contactClass c = new contactClass();
+        static string myconnstrng = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Get the value from the input fields
+            c.FirstName = txtboxFirstName.Text;
+            c.LastName = txtboxLastName.Text;
+            c.ContactNo = txtboxContactNumber.Text;
+            c.Address = txtboxAddress.Text;
+            c.Gender = cmbGender.Text;
+
+            //Inserting Data into Database
+            bool success = c.Insert(c);
+            if (success == true)
+            {
+                MessageBox.Show("New Contact Successfully Inserted");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("failed to Add New Contact. Try again!");
+            }
+
+            DataTable dt = c.Select();
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+        
+        private void DataGrid_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            SqlConnection sqlconn = new SqlConnection(myconnstrng);
+            try
+            {
+                sqlconn.Open();
+                string Query = "select FirstName, LastName, ContactNo, Address, Gender from tbl_contact";
+                SqlCommand createCommand = new SqlCommand(Query, sqlconn);
+                createCommand.ExecuteNonQuery();
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(createCommand);
+                DataTable dt = new DataTable("tbl_contact");
+                dataAdp.Fill(dt);
+                dataGrid.ItemsSource = dt.DefaultView;
+                dataAdp.Update(dt);
+                sqlconn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlconn = new SqlConnection(myconnstrng);
+            try
+            {
+                sqlconn.Open();
+                string Query = "select FirstName, LastName, ContactNo, Address, Gender from tbl_contact";
+                SqlCommand createCommand = new SqlCommand(Query, sqlconn);
+                createCommand.ExecuteNonQuery();
+
+                SqlDataAdapter dataAdp = new SqlDataAdapter(createCommand);
+                DataTable dt = new DataTable("tbl_contact");
+                dataAdp.Fill(dt);
+                dataGrid.ItemsSource = dt.DefaultView;
+                dataAdp.Update(dt);
+                sqlconn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Econtact_Load(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = c.Select();
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+
+        public void Clear()
+        {
+            txtboxFirstName.Text = "";
+            txtboxLastName.Text = "";
+            txtboxContactNumber.Text = "";
+            txtboxAddress.Text = "";
+            cmbGender.Text = "";
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            c.ContactID = int.Parse(txtboxContactID.Text);
+            c.FirstName = txtboxFirstName.Text;
+            c.LastName = txtboxLastName.Text;
+            c.ContactNo = txtboxAddress.Text;
+            c.Address = txtboxAddress.Text;
+            c.Gender = cmbGender.Text;
+            bool success = c.Update(c);
+            if (success == true)
+            {
+                MessageBox.Show("Contact has been successfully Updated.");
+            }
+            else
+            {
+                MessageBox.Show("failed to Update Contact.Try Again");
+            }
+        }
+
+        private void Row_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            // Ensure row was clicked and not empty space
+            var row = ItemsControl.ContainerFromElement((DataGrid)sender,
+                                                e.OriginalSource as DependencyObject) as DataGridRow;
+
+            if (row == null)
+                
+            return;
+
+        }
+    }
+}
---
+++ contactClass.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Econtact
+{
+    class contactClass
+    {
+        //Getter Setter Properties
+        //Acts as Data Carrier in our application
+        public int ContactID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNo { get; set; }
+        public string Address { get; set; }
+        public string Gender { get; set; }
+
+        static string myconnstrng=ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+    
+        //Selecting Data from Database 
+    public DataTable Select()
+    {
+        //Step 1: Database Connection 
+        SqlConnection conn = new SqlConnection(myconnstrng);
+        DataTable dt = new DataTable();
+        try
+        {
+                //Step 2: Writing SQL Query 
+            string sql = "SELECT * FROM tbl_contact";
+                //Creating cmd using sql and conn
+            SqlCommand cmd = new SqlCommand(sql, conn);
+                //Creating SQL DataAdapter using cmd
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            conn.Open();
+            adapter.Fill(dt);
+        }
+        catch (Exception)
+            {
+           
+        }
+        finally
+        {
+            conn.Close();
+        }
+        return dt;
+    }
+        //Inserting Datainto Database 
+    public bool Insert(contactClass c)
+    {
+            //Creating a default return type and setting its value to false 
+        bool isSuccess = false;
+            //Step 1: Connect Database 
+        SqlConnection conn = new SqlConnection(myconnstrng);
+            try
+        {
+            //Step 2: Create a SQL Query to insert Data 
+            string sql = "INSERT INTO tbl_contact (FirstName, LastName, ContactNo, Address, Gender) VALUES (@FirstName, @LastName, @ContactNo, @Address, @Gender)";
+            //Creating SQL Command using sql and conn 
+                SqlCommand cmd = new SqlCommand(sql,conn);
+                //Create Parameters to add data 
+            cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", c.LastName);
+            cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+            cmd.Parameters.AddWithValue("@Address", c.Address);
+            cmd.Parameters.AddWithValue("@Gender", c.Gender);
+                //Connection Open here 
+            conn.Open();
+            int rows = cmd.ExecuteNonQuery();
+                //If the query runs successfully then the value of rows will be greater than zero else its value will be 0
+            if (rows>0)
+            {
+                isSuccess = true;
+            }
+            else
+            {
+                isSuccess = false;
+            }
+        }
+        catch (Exception)
+            {
+
+        }
+        finally
+        {
+            conn.Close();
+        }
+        return isSuccess;
+        }
+        //Method to update data in database from our application 
+public bool Update(contactClass c)
+{
+            //Create a default return type and set its default value to false 
+    bool isSuccess = false;
+        SqlConnection conn = new SqlConnection(myconnstrng);
+    try
+    {
+                //SQL to update data in our Database
+            string sql = "UPDATE tbl_contact SET FirstName=@FirstName, LastName=@LastName, ContactNo=@ContactNo, Address=@Address, Gender=@Gender WHRER Contact=@ContactID";
+           
+                //Creating SQL Command 
+            SqlCommand cmd = new SqlCommand(sql, conn);
+                //Create Parameters to add value 
+            cmd.Parameters.AddWithValue("@FirstName", c.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", c.LastName);
+            cmd.Parameters.AddWithValue("@ContactNo", c.ContactNo);
+            cmd.Parameters.AddWithValue("@Adress", c.Address);
+            cmd.Parameters.AddWithValue("@Gender", c.Gender);
+            cmd.Parameters.AddWithValue("@ContactID", c.ContactID);
+            //Open Database Connection 
+            conn.Open();
+            int rows = cmd.ExecuteNonQuery();
+                //if the query runs succfully then the value of rows will be 0
+            if (rows>0)
+            {
+                isSuccess = true;
+            }
+            else
+            {
+                isSuccess = false;
+            }
+        }
+    catch (Exception)
+            {
+
+    }
+    finally
+    {
+            conn.Close();
+    }
+        return isSuccess;
+    }
+        //Method to Delete Data from Database
+ public bool Delete(contactClass c)
+    {
+            //Create a default return value and set its value to false 
+        bool isSuccess = false;
+            //Create SQL Connection
+        SqlConnection conn = new SqlConnection(myconnstrng);
+        try
+        {
+                //SQL To Delete Data 
+            string sql = "DELETE FROM tbl_contact WHERE ContactID=@ContactID";
+           //Creating SQL Command
+                SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@ContactID", c.ContactID);
+            //Open Connection
+                conn.Open();
+            int rows = cmd.ExecuteNonQuery();
+                //if the query run secessfully then the value of rows is greater than 0 else its value is 0
+            if (rows>0)
+            {
+                isSuccess = true;
+            }
+            else
+            {
+                isSuccess = false;
+            }
+        }
+        catch (Exception)
+            {
+          
+        }
+        finally
+        {
+//Close Connection 
+            conn.Close();
+        }
+            return isSuccess;
+    }
+    }
+}
